9ba0ebd76363668b3254dc43ad10d27d7df0bb4f
isabelxklee <hi@kleetime.com>
2025-04-23 11:06:26 -0400

    created route for pdf parser

---
+++ src/api/upload/route.tsx
@@ -0,0 +1,18 @@
+import fs from "fs";
+import PDFParser from "pdf2json";
+
+const pdfParser = new PDFParser();
+
+pdfParser.on("pdfParser_dataError", (errData) =>
+  console.error(errData.parserError)
+);
+
+pdfParser.on("pdfParser_dataReady", (pdfData) => {
+  fs.writeFile(
+    "./pdf2json/test/F1040EZ.json",
+    JSON.stringify(pdfData),
+    (data) => console.log(data)
+  );
+});
+
+pdfParser.loadPDF("./pdf2json/test/pdf/fd/form/F1040EZ.pdf");
