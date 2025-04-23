d833c7697bb1ff67d20471322147c892c2f1685f
isabelxklee <hi@kleetime.com>
2025-04-23 11:00:27 -0400

    added filepond component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { useState } from "react";
+import { FilePond } from "react-filepond";
+import "filepond/dist/filepond.min.css";
 
 export default function Home() {
-  const [file, setFile] = useState<File | null>(null);
+  const [files, setFiles] = useState<any[]>([]);
 
-  console.log(file);
+  console.log(files);
 
-  return <></>;
+  return (
+    <>
+      <FilePond
+        files={files}
+        onupdatefiles={setFiles}
+        allowMultiple={true}
+        maxFiles={3}
+        server="/api"
+        name="files" /* sets the file input name, it's filepond by default */
+        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
+    </>
+  );
 }
